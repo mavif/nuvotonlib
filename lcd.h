58d52b9bcacfbd615867566d3502dc0507f34e0f
ngemilchiki <saya.avif@gmail.com>
2016-04-18 06:31:52 +0700

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -1,7 +1,7 @@
 #ifndef _lcd_h_
 #define _lcd_h_
 extern void lcd(void);
-extern void light(void);
-extern void printlcd(unsigned char line, char *str);
-extern void clrlcd(void);
+extern void backlight(void);
+extern void text(unsigned char line, char *str);
+extern void clear(void);
 #endif
